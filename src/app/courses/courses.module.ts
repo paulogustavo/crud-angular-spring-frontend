ac699755de1da98f30602507452d078e3e2c0630
Paulo Gustavo <paulogustavo.seap.ma@gmail.com>
2023-06-21 15:32:33 -0300

    feat: added courses toolbar

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatTableModule } from '@angular/material/table';
+import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 import { CoursesRoutingModule } from './courses-routing.module';
 import { CoursesComponent } from './courses/courses.component';
@@ -13,7 +15,9 @@ import { CoursesComponent } from './courses/courses.component';
   imports: [
     CommonModule,
     CoursesRoutingModule,
-    MatTableModule
+    MatTableModule,
+    MatCardModule,
+    MatToolbarModule
   ]
 })
 export class CoursesModule { }
